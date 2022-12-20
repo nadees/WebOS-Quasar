ff472ca289b948e2d5db85fc64b81f9cb9e8ec2c
nadees <nadeeshafernando2020@gmail.com>
2022-12-20 23:25:52 +0530

    Update: Special for developers.
    I added "maximize" and "minimize" buttons to the main menu bar.
    This might help your development task and I made some code changes to "default.css", "default.js" and "WebOS.html".

---
+++ system/js/default.js
@@ -0,0 +1,22 @@
+document.addEventListener('contextmenu', event => event.preventDefault());
+
+var elem = document.documentElement;
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) { /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE11 */
+    elem.msRequestFullscreen();
+  }
+}
+
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.webkitExitFullscreen) { /* Safari */
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) { /* IE11 */
+    document.msExitFullscreen();
+  }
+}
