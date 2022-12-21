dff3a207ac4df2aa99546c25205c64cb123cdd15
nadees <nadeeshafernando2020@gmail.com>
2022-12-21 15:11:57 +0530

    Update: Context menu added

---
+++ system/js/context-menu.js
@@ -0,0 +1,13 @@
+document.addEventListener('contextmenu', function(event) {
+    event.preventDefault();
+    var contextMenu = document.querySelector('.context-menu');
+    contextMenu.style.top = event.clientY + 'px';
+    contextMenu.style.left = event.clientX + 'px';
+    contextMenu.classList.add('active');
+  });
+  
+  document.addEventListener('click', function() {
+    var contextMenu = document.querySelector('.context-menu');
+    contextMenu.classList.remove('active');
+  });
+  
