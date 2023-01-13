50ab97c25531d69aefd41cc69139459d22525802
nadees <nadeeshafernando2020@gmail.com>
2023-01-13 13:21:17 +0530

    Update: Application Manager 'appmgr.js'

--- system/apps/appmgr.js
+++ system/apps/appmgr.js
@@ -1,12 +1,53 @@
+function app_files() {
+    var element = document.getElementById("window");
+    element.style.display = "block";
+    document.getElementById("window_content").src = "/system/apps/files/files.html";
+}
+
 function app_calc() {
     var element = document.getElementById("window");
     element.style.display = "block";
     document.getElementById("window_content").src = "/system/apps/calc/calc.html";
 }
 
+function app_calender() {
+    var element = document.getElementById("window");
+    element.style.display = "block";
+    document.getElementById("window_content").src = "/system/apps/calender/calender.html";
+}
+
+function app_cam() {
+    var element = document.getElementById("window");
+    element.style.display = "block";
+    document.getElementById("window_content").src = "/system/apps/cam/cam.html";
+}
+
+function app_clock() {
+    var element = document.getElementById("window");
+    element.style.display = "block";
+    document.getElementById("window_content").src = "/system/apps/clock/clock.html";
+}
+
 function app_browser() {
     var element = document.getElementById("window");
     element.style.display = "block";
-    document.getElementById("window_content").src = "https://usproxy.vpnbook.com/browse.php";
+    document.getElementById("window_content").src = "/system/apps/browser/browser.html";
 }
 
+function app_photos() {
+    var element = document.getElementById("window");
+    element.style.display = "block";
+    document.getElementById("window_content").src = "/system/apps/photos/photos.html";
+}
+
+function app_terminal() {
+    var element = document.getElementById("window");
+    element.style.display = "block";
+    document.getElementById("window_content").src = "/system/apps/terminal/terminal.html";
+}
+
+function app_notebook() {
+    var element = document.getElementById("window");
+    element.style.display = "block";
+    document.getElementById("window_content").src = "/system/apps/notebook/notebook.html";
+}
