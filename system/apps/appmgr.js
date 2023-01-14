232a850f25963e084f000429dafda7194d1b59ed
nadees <nadeeshafernando2020@gmail.com>
2023-01-15 02:00:22 +0530

    Update: Calculator
    More reliable application.
    Google Calculator from CodePen.
    And Application Manager title settings.

--- system/apps/appmgr.js
+++ system/apps/appmgr.js
@@ -2,52 +2,61 @@ function app_files() {
     var element = document.getElementById("window");
     element.style.display = "block";
     document.getElementById("window_content").src = "/system/apps/files/files.html";
+    document.getElementById("window_title").innerHTML = "Files";
 }
 
 function app_calc() {
     var element = document.getElementById("window");
     element.style.display = "block";
     document.getElementById("window_content").src = "/system/apps/calc/calc.html";
+    document.getElementById("window_title").innerHTML = "Calculator";
 }
 
 function app_calender() {
     var element = document.getElementById("window");
     element.style.display = "block";
     document.getElementById("window_content").src = "/system/apps/calender/calender.html";
+    document.getElementById("window_title").innerHTML = "Calender";
 }
 
 function app_cam() {
     var element = document.getElementById("window");
     element.style.display = "block";
     document.getElementById("window_content").src = "/system/apps/cam/cam.html";
+    document.getElementById("window_title").innerHTML = "Camera";
 }
 
 function app_clock() {
     var element = document.getElementById("window");
     element.style.display = "block";
     document.getElementById("window_content").src = "/system/apps/clock/clock.html";
+    document.getElementById("window_title").innerHTML = "Clock";
 }
 
 function app_browser() {
     var element = document.getElementById("window");
     element.style.display = "block";
     document.getElementById("window_content").src = "/system/apps/browser/browser.html";
+    document.getElementById("window_title").innerHTML = "Browser";
 }
 
 function app_photos() {
     var element = document.getElementById("window");
     element.style.display = "block";
     document.getElementById("window_content").src = "/system/apps/photos/photos.html";
+    document.getElementById("window_title").innerHTML = "Photos";
 }
 
 function app_terminal() {
     var element = document.getElementById("window");
     element.style.display = "block";
     document.getElementById("window_content").src = "/system/apps/terminal/terminal.html";
+    document.getElementById("window_title").innerHTML = "Terminal";
 }
 
 function app_notebook() {
     var element = document.getElementById("window");
     element.style.display = "block";
     document.getElementById("window_content").src = "/system/apps/notebook/notebook.html";
+    document.getElementById("window_title").innerHTML = "Notebook";
 }
