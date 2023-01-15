734e79de5bc4578458229bb7546443ba8c2768ed
nadees <nadeeshafernando2020@gmail.com>
2023-01-15 21:08:54 +0530

    Update: System internal file setup and system improvements.
    We made a space for users to save their information called "pool".
    
    And we create a new file called "$dir-info.xml" to store properties and settings.
    
    Further more we made "appmgr.js" more reliable.

--- system/apps/appmgr.js
+++ system/apps/appmgr.js
@@ -2,35 +2,35 @@ function app_files() {
     var element = document.getElementById("window");
     element.style.display = "block";
     document.getElementById("window_content").src = "/system/apps/files/files.html";
-    document.getElementById("window_title").innerHTML = "Files";
+    document.getElementById("window_title").innerHTML = "📁 Files";
 }
 
 function app_calc() {
     var element = document.getElementById("window");
     element.style.display = "block";
     document.getElementById("window_content").src = "/system/apps/calc/calc.html";
-    document.getElementById("window_title").innerHTML = "Calculator";
+    document.getElementById("window_title").innerHTML = "🧮 Calculator";
 }
 
 function app_calender() {
     var element = document.getElementById("window");
     element.style.display = "block";
     document.getElementById("window_content").src = "/system/apps/calender/calender.html";
-    document.getElementById("window_title").innerHTML = "Calender";
+    document.getElementById("window_title").innerHTML = "🗓️ Calender";
 }
 
 function app_cam() {
     var element = document.getElementById("window");
     element.style.display = "block";
     document.getElementById("window_content").src = "/system/apps/cam/cam.html";
-    document.getElementById("window_title").innerHTML = "Camera";
+    document.getElementById("window_title").innerHTML = "📸 Camera";
 }
 
 function app_clock() {
     var element = document.getElementById("window");
     element.style.display = "block";
     document.getElementById("window_content").src = "/system/apps/clock/clock.html";
-    document.getElementById("window_title").innerHTML = "Clock";
+    document.getElementById("window_title").innerHTML = "⏰ Clock";
 }
 
 function app_browser() {
@@ -38,33 +38,33 @@ function app_browser() {
     element.style.display = "block";
     element.style.overflow = "hidden";
     document.getElementById("window_content").src = "/system/apps/browser/browser.html";
-    document.getElementById("window_title").innerHTML = "Browser";
+    document.getElementById("window_title").innerHTML = "🌐 Browser";
 }
 
 function app_photos() {
     var element = document.getElementById("window");
     element.style.display = "block";
     document.getElementById("window_content").src = "/system/apps/photos/photos.html";
-    document.getElementById("window_title").innerHTML = "Photos";
+    document.getElementById("window_title").innerHTML = "🏳️‍🌈 Photos";
 }
 
 function app_terminal() {
     var element = document.getElementById("window");
     element.style.display = "block";
     document.getElementById("window_content").src = "/system/apps/terminal/terminal.html";
-    document.getElementById("window_title").innerHTML = "Terminal";
+    document.getElementById("window_title").innerHTML = "🧊 Terminal";
 }
 
 function app_notebook() {
     var element = document.getElementById("window");
     element.style.display = "block";
     document.getElementById("window_content").src = "/system/apps/notebook/notebook.html";
-    document.getElementById("window_title").innerHTML = "Notebook";
+    document.getElementById("window_title").innerHTML = "📝 Notebook";
 }
 
 function app_settings() {
     var element = document.getElementById("window");
     element.style.display = "block";
     document.getElementById("window_content").src = "/system/apps/settings/settings.html";
-    document.getElementById("window_title").innerHTML = "Settings";
+    document.getElementById("window_title").innerHTML = "🔮 Settings";
 }
